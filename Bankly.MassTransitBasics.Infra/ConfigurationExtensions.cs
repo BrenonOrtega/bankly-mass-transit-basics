9373f7e4c7bcb05e4b24fcbc898e9748daae4ab6
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-09-22 20:44:38 -0300

    add redis/masstransit config & queueSettings

--- Bankly.MassTransitBasics.Infra/ConfigurationExtensions.cs
+++ Bankly.MassTransitBasics.Infra/ConfigurationExtensions.cs
@@ -1,3 +1,4 @@
+using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using StackExchange.Redis;
@@ -7,12 +8,15 @@ namespace Bankly.MassTransitBasics.Infra
 {
     public static class ConfigurationExtensions
     {
+        public static IConfigurationBuilder AddQueueSettings(this IConfigurationBuilder builder) => 
+            builder.AddJsonFile("queueSettings.json", optional: false, reloadOnChange:true);
+        
         public static IServiceCollection AddRedisRepository(this IServiceCollection services, IConfiguration config)
         {
             services.AddStackExchangeRedisCache(options =>
             {
-                var defaultDb = config["redis:database"] ?? throw new ArgumentException("database not defined", nameof(ConfigurationOptions.DefaultDatabase));
-                
+                var defaultDb = config["Redis:Database"] ?? throw new ArgumentException("database not defined", nameof(ConfigurationOptions.DefaultDatabase));
+
                 options.Configuration = config.GetConnectionString("redis");
                 options.ConfigurationOptions = new ConfigurationOptions()
                 {
@@ -21,6 +25,27 @@ namespace Bankly.MassTransitBasics.Infra
             });
 
             services.AddScoped(typeof(IRepository<>), typeof(RedisRepository<>));
+            return services;
+        }
+
+        public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services, IConfiguration config, bool useHostedServices)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(config["RabbitMq:Hostname"], host =>
+                    {
+                        host.Password(config["RabbitMq:Password"]);
+                        host.Username(config["RabbitMq:Username"]);
+                    });
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+
+            if (useHostedServices)
+                services.AddMassTransitHostedService();
+
             return services;
         }
     }
