dedd95d7010c8a1385e234b9142dee137eac99fc
BrenonOrtega <71930739+BrenonOrtega@users.noreply.github.com>
2021-09-23 09:29:53 -0300

    Adds QueueSettings extension MassTransit Consumer.

--- Bankly.MassTransitBasics.Infra/ConfigurationExtensions.cs
+++ Bankly.MassTransitBasics.Infra/ConfigurationExtensions.cs
@@ -3,35 +3,38 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using StackExchange.Redis;
 using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Bankly.MassTransitBasics.Infra
 {
     public static class ConfigurationExtensions
     {
-        public static IConfigurationBuilder AddQueueSettings(this IConfigurationBuilder builder) => 
-            builder.AddJsonFile("queueSettings.json", optional: false, reloadOnChange:true);
-        
+        public static IConfigurationBuilder AddQueueSettings(this IConfigurationBuilder builder) =>
+            builder.AddJsonFile("queueSettings.json", optional: false, reloadOnChange: true);
+
         public static IServiceCollection AddRedisRepository(this IServiceCollection services, IConfiguration config)
         {
             services.AddStackExchangeRedisCache(options =>
             {
                 var defaultDb = config["Redis:Database"] ?? throw new ArgumentException("database not defined", nameof(ConfigurationOptions.DefaultDatabase));
 
+                options.InstanceName = "bankly-demo-redis";
                 options.Configuration = config.GetConnectionString("redis");
-                options.ConfigurationOptions = new ConfigurationOptions()
-                {
-                    DefaultDatabase = Int32.Parse(defaultDb)
-                };
+                options.ConfigurationOptions = new ConfigurationOptions() { };
             });
 
             services.AddScoped(typeof(IRepository<>), typeof(RedisRepository<>));
             return services;
         }
 
-        public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services, IConfiguration config, bool useHostedServices)
+        public static IServiceCollection AddMassTransitWithRabbitMq(this IServiceCollection services, IConfiguration config, Assembly[] assemblies, bool useHostedServices)
         {
             services.AddMassTransit(x =>
             {
+                x.SetKebabCaseEndpointNameFormatter();
+                x.AddConsumers(assemblies);
+                
                 x.UsingRabbitMq((context, cfg) =>
                 {
                     cfg.Host(config["RabbitMq:Hostname"], host =>
--- Bankly.MassTransitBasics.Infra/RedisRepository.cs
+++ Bankly.MassTransitBasics.Infra/RedisRepository.cs
@@ -3,6 +3,8 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
 using StackExchange.Redis;
 
 namespace Bankly.MassTransitBasics.Infra
@@ -10,20 +12,21 @@ namespace Bankly.MassTransitBasics.Infra
     public class RedisRepository<T> : IRepository<T>
     {
         private readonly IDatabase _database;
-        public RedisRepository(IDatabase database)
+        public RedisRepository(IConfiguration config)
         {
-            _database = database ?? throw new ArgumentException(nameof(database));
+            var database = config["Redis:Database"] ?? throw new ArgumentException("Config[\"Redis:Database\"]");
+            _database = ConnectionMultiplexer.Connect(config.GetConnectionString("Redis")).GetDatabase(int.Parse(database));
         }
 
         public Task AddAsync<V>(V storeKey, T entity)
         {
-            var serializedData = JsonSerializer.Serialize(entity, new JsonSerializerOptions { WriteIndented = true });
-            return _database.StringAppendAsync(new RedisKey(storeKey.ToString()), serializedData);
+            var serializedData = JsonSerializer.SerializeToUtf8Bytes(entity, new JsonSerializerOptions { WriteIndented = true });
+            return _database.SetAddAsync(storeKey.ToString(), serializedData);
         }
 
         public async Task<T> GetOneAsync<V>(V storeKey)
         {
-            var data = await _database.StringGetAsync(new RedisKey(storeKey.ToString()));
+            var data = await _database.StringGetAsync(storeKey.ToString());
             var deserializedEntity = JsonSerializer.Deserialize<T>(data);
             return deserializedEntity;
         }
